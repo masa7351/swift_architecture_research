4c6f579f7884796cc8f0e09c0d6427ba1ad833a2
Masanao Imai <masa7351@gmail.com>
2020-02-07 15:35:50 +0900

    Change iOS target version from 13 to 11

--- Viper/Viper/AppDelegate.swift
+++ Viper/Viper/AppDelegate.swift
@@ -10,19 +10,30 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        true
+        if #available(iOS 13, *) {
+        } else {
+            let window = UIWindow(frame: UIScreen.main.bounds)
+            self.window = window
+            window.makeKeyAndVisible()
+
+            let vc = SearchUserViewController.instantiate()
+            window.rootViewController = vc
+        }
+        return true
     }
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_: UIApplication, didDiscardSceneSessions _: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
